f1a89c2476623d4d5940652a963dcde4e8e04568
Robert Dixon <attrition242@gmail.com>
2019-03-28 18:00:26 -0400

    rewrite in 1.0.0a
    rewrite for discord.py 1.0.0a

--- README.md
+++ README.md
@@ -2,13 +2,11 @@
 
 ##  This application is a discord bot wrote in Python that allows control of a token currency that is currently maintained in a CSV file.
 
-In order to use this bot, duplicate the .env.example and rename it to .env and then import your correct token into the variable DISCORD_TOKEN='Token'
+In order to use this bot, go to the private_key file and replace "token" with the token given to you by discord
 
-You will need to make that token available globally for the CLI to be able to access the variable os wide.
-
-You will also need to install Discord via PIP.
+You will also need to install Discord 1.0.0a via PIP. Do not pip install discord.py as it will install 0.16.x as of 3/28/2019, instead please use the command "python3 -m pip install -U git+https://github.com/Rapptz/discord.py@rewrite"
 
 You can reference this documentation for that.
-https://github.com/Rapptz/discord.py
+https://discordpy.readthedocs.io/en/rewrite/
 
 You can then run the application.
--- main.py
+++ main.py
@@ -1,136 +1,32 @@
 import discord
+from discord.ext import commands
+import platform
 import asyncio
-import os
 import sqlite3
+import private_key
 
 
-client = discord.Client()
-DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
+cog_list = ["cogs.currency"]
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+class DragonHoard(commands.Bot):
+    def __init__(self):
+        super().__init__(description="DragonHoard", command_prefix="!", case_insensitive=True, pm_help=False)
+        for cog in cog_list:
+            self.load_extension(cog)
 
-@client.event
-async def on_message(message):
-    # TODO: Confirm it's the same user if they go through the giving cycle in each step
-    # TODO: Make this more oop friendly or functional and less procedural for legibility sake
-
-    # Show help message when the !credits command is entered
-    if message.content.lower() == '!credits':
-        await client.send_message(message.channel, 'You\'re currently missing parameters in your request.\n\n' +
-                                                   'In order to properly respond we need the following information from your query.\n\n' +
-                                                   '(1)!credits (2)Requested Operation (3)Targeted User (4)Credit Amount\n' +
-                                                   'Requested operations are [Give, Request, Destroy, Rob]')
-
-    if message.content.lower().startswith('!credits'):
-        # explode the given statement
-        user_input = message.content.lower().split(" ")
-
-        # TODO: Change this to a correct loop instead of a series of ifs
-        # Default values here if they don't exist
-        user_input_length = len(user_input)
-        default_inputs = [None, None, 0]
-        if user_input_length <= 1:
-            user_input.append(None)
-        if user_input_length <= 2:
-            user_input.append(None)
-        if user_input_length <= 3:
-            user_input.append(0)
-
-        # create command based dictionary to work off of
-        credit_operation = {
-            "author": message.author,
-            "author_balance": 0,
-            "operator": user_input[1],
-            "tagged_user": user_input[2],
-            "tagged_balance": 0,
-            "credit_amount": user_input[3]
-        }
-
-        # NOTE: Should actually be using sqlite https://docs.python.org/3.7/library/sqlite3.html
-        # https://docs.python.org/3.7/library/sqlite3.html#sqlite3.Cursor
-        # open the credit vault obtain their credit balance
-        credit_vault = sqlite3.connect('creditVault.db')
-        cursor = credit_vault.cursor()
-        # TODO: Actually locate the authors balance
-        author_balance = cursor.executescript("SELECT * WHERE user_id = credit_operation['author']")
-        # TODO: If no balance exists then default the balance to 0 and create a new record in the CSV
+    async def on_ready(self):  # Tells us info about the bot and readys the bot for taking commands
+        print(f'Logged in as {self.user.name} (ID:{self.user.id}')
+        # Username and ID
+        print('--------')
+        print(
+            f'Current Discord.py Version: {discord.__version__} | Current Python Version: {platform.python_version()}')
+        # Discord and Python version
+        print('--------')
+        print(f'Use this link to invite {self.user.name}:')
+        print(f'https://discordapp.com/oauth2/authorize?client_id={self.user.id}&scope=bot&permissions=8')
+        # prints a link that you may use to invite to your server
 
 
-        # obtain users credit balance
-        credit_operation["author_balance"] = author_balance
-
-        # TODO: There is potential to optimize it with a python equivalent of this http://es6-features.org/#RestParameter
-        # if second param doesnt exist ask 'what would you like do with credits? Options: Give, Request, Destroy, Rob'
-        if (
-                credit_operation['operator'] is None or (
-                credit_operation['operator'] != 'give'
-                or credit_operation['operator'] != 'request'
-                or credit_operation['operator'] != 'destroy'
-                or credit_operation['operator'] != 'rob')
-        ):
-            await client.send_message(message.channel, 'Invalid operation parameter, transaction ended.')
-            return
-
-        # if third param doesn't exist ask 'who would you like' second param ' credits to or from?'
-        if (
-                credit_operation['tagged_user'] is None or (
-                credit_operation['tagged_user'].startswith("<@")
-                and credit_operation['tagged_user'].endswith(">")
-                and len(credit_operation['tagged_user']) == 21)
-        ):
-            await client.send_message(message.channel, 'Invalid transaction recipient, transaction ended.')
-            return
-
-        # TODO: Actually locate the authors balance
-        tagged_balance = credit_vault.read()
-        # TODO: If no balance exists then default the balance to 0 and create a new record in the CSV
-        credit_operation["tagged_balance"] = tagged_balance
-
-#     if fourth param doesn't exist
-        if isinstance(credit_operation['credit_amount'], int):
-            # return 'Transaction ended, invalid credit amount.'
-            await client.send_message(message.channel, 'Transaction ended, invalid credit amount.')
-            return
-
-#   check the second param against a switch statement
-#     case give
-#       if creditBalance - creditAmount >= 0
-#         if credit_operation['author_balance'] - credit_operation['credit_amount'] >= 0:
-
-#         process csv request against the third param
-#         return 'Transaction completed. Remaining balance: ' creditBalance - creditAmount
-#             await client.send_message(message.channel, 'Transaction completed. Remaining balance ' + (credit_operation['author_balance'] - credit_operation['credit_amount']))
-#       else
-#         return 'Insufficient credit balance to process transaction.'
-#     case request
-#       if snowflakeUserId.creditBalance - creditAmount >= 0
-#         apply response message to chat 'Attention to ' @snowflakeUserId '
-#         would you like to proccess this transaction? Y/N'
-#         wait for a response from given @snowflakeUserId
-#         if message.content is not Y
-#           return 'Transaction has been declined by ' @snowflakeUserId
-#         process csv request against the user who made the request
-#         return 'Transaction completed. Remaining balance: ' creditBalance - creditAmount
-#       else
-#         return 'Insufficient credit balance to process transaction.'
-#     case destroy
-#     if creditBalance - creditAmount >= 0
-#         process csv request against the user who made the request
-#         return 'Transaction completed. Remaining balance: ' creditBalance - creditAmount
-#       else
-#         return 'Insufficient credit balance to process transaction.'
-#     case rob
-#       check to see if user is capable of robbing user.lastRobbery
-#       check to see if snowflakeUserId is capable of being robbed snowflakeUserId.lastRobbed
-#       attemptedNumber roll some odds 1 in creditamount to rob said user
-#       if randInt numbers between (1, creditAmount) == attemptedNumber
-#         return 'Well, there\'s been a robbery, ' @snowflakeUserId ' has been robbed for ' attemptedNumber '.'
-#       else
-#         return 'Better luck next time, it was a 1 out of ' attemptedNumber ' it\'s all RNG'
-#       some zany logic that will use the csv to apply a time date for last attempted robbery and lastRobbed
-client.run(DISCORD_TOKEN)
+if __name__ == "__main__":
+    client = DragonHoard()
+    client.run(private_key.private_key)
